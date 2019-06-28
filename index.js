175d32ac6812646f9b12fde16812ea983ffa98eb
Jay Li <jayli3n@gmail.com>
2019-06-29 01:04:13 +0800

    customized

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
-import React, { Component } from "react";
+/**
+Original creators: https://github.com/App2Sales/react-native-switch-selector
+Modified for my own use to allow for custom fonts, sizes and colors
+**/
+import React, { Component } from 'react';
 import {
   Animated,
   Easing,
@@ -8,29 +12,35 @@ import {
   Text,
   TouchableOpacity,
   View
-} from "react-native";
+} from 'react-native';
+import {
+  colorPicker,
+  fontSizePicker,
+  fontFamilyPicker
+} from './helper';
+
 
 const styles = {
   button: {
     flex: 1,
-    flexDirection: "row",
-    justifyContent: "center",
-    alignItems: "center"
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   containerButton: {
-    flexDirection: "row",
+    flexDirection: 'row',
     flex: 1,
     height: 40,
-    justifyContent: "center",
-    alignItems: "center"
+    justifyContent: 'center',
+    alignItems: 'center'
   },
   animated: {
     borderWidth: 0,
-    position: "absolute"
+    position: 'absolute'
   }
 };
 
-export default class SwitchSelector extends Component {
+export default class AwsmSelector extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -60,40 +70,40 @@ export default class SwitchSelector extends Component {
     }
   }
 
+  getBgColor(bgColor) {
+    const { selected } = this.state;
+    const { options, buttonColor } = this.props;
+    return options[selected].activeColor || buttonColor || bgColor;
+  }
+
+  _getSwipeDirection(gestureState) {
+    const { dx, dy, vx } = gestureState;
+    // 0.1 velocity
+    if (Math.abs(vx) > 0.1 && Math.abs(dy) < 80) {
+      return dx > 0 ? 'RIGHT' : 'LEFT';
+    }
+    return null;
+  }
+
+  responderEnd = (evt, gestureState) => {
+    if (this.props.disabled) return;
+    const swipeDirection = this._getSwipeDirection(gestureState);
+    if (
+      swipeDirection === 'RIGHT' &&
+      this.state.selected < this.props.options.length - 1
+    ) {
+      this.toggleItem(this.state.selected + 1);
+    } else if (swipeDirection === 'LEFT' && this.state.selected > 0) {
+      this.toggleItem(this.state.selected - 1);
+    }
+  };
+
   shouldSetResponder = (evt, gestureState) => {
     return (
       evt.nativeEvent.touches.length === 1 &&
       !(Math.abs(gestureState.dx) < 5 && Math.abs(gestureState.dy) < 5)
     );
   };
-
-  responderEnd = (evt, gestureState) => {
-    if (this.props.disabled) return;
-    const swipeDirection = this._getSwipeDirection(gestureState);
-    if (
-      swipeDirection === "RIGHT" &&
-      this.state.selected < this.props.options.length - 1
-    ) {
-      this.toggleItem(this.state.selected + 1);
-    } else if (swipeDirection === "LEFT" && this.state.selected > 0) {
-      this.toggleItem(this.state.selected - 1);
-    }
-  };
-
-  _getSwipeDirection(gestureState) {
-    const { dx, dy, vx } = gestureState;
-    // 0.1 velocity
-    if (Math.abs(vx) > 0.1 && Math.abs(dy) < 80) {
-      return dx > 0 ? "RIGHT" : "LEFT";
-    }
-    return null;
-  }
-
-  getBgColor() {
-    const { selected } = this.state;
-    const { options, buttonColor } = this.props;
-    return options[selected].activeColor || buttonColor;
-  }
 
   animate = (value, last) => {
     this.animatedValue.setValue(last);
@@ -117,7 +127,7 @@ export default class SwitchSelector extends Component {
     if (callOnPress && onPress) {
       onPress(returnObject ? options[index] : options[index].value);
     } else {
-      console.log("Call onPress with value: ", options[index].value);
+      console.log('AwsmSelector onPress with value: ', options[index].value);
     }
     this.setState({ selected: index });
   };
@@ -130,16 +140,30 @@ export default class SwitchSelector extends Component {
       imageStyle,
       textColor,
       selectedColor,
-      fontSize,
+      textFontSize,
+      textLineHeight,
+      textFontFamily,
       backgroundColor,
       borderColor,
       borderRadius,
-      hasPadding,
-      valuePadding,
-      height,
-      bold,
-      disabled
+      borderWidth,
+      disabled,
+      size,
+      buttonHeight,
+      tight,
+      rounded,
+      type
     } = this.props;
+
+    // Modifying color of button
+    const { fgColor, bgColor, fgColorInverse } = colorPicker(type);
+    // Get font size and line height based on size prop
+    const { fFamily } = fontFamilyPicker(14); // 14 is button font
+    const { fSize, lHeight } = fontSizePicker(size);
+    const fontSize = textFontSize || fSize;
+    const lineHeight = textLineHeight || lHeight;
+    // Working out the the padding and height of btn based on content size
+    const height = buttonHeight || fontSize * 3;
 
     const options = this.props.options.map((element, index) => (
       <TouchableOpacity
@@ -147,9 +171,10 @@ export default class SwitchSelector extends Component {
         disabled={disabled}
         style={styles.button}
         onPress={() => this.toggleItem(index)}
+        activeOpacity={0.5}
       >
-        {typeof element.customIcon === "function"
-          ? element.customIcon(this.state.selected == index)
+        {typeof element.customIcon === 'function'
+          ? element.customIcon(this.state.selected === index)
           : element.customIcon}
         {element.imageIcon && (
           <Image
@@ -159,7 +184,8 @@ export default class SwitchSelector extends Component {
                 height: 30,
                 width: 30,
                 tintColor:
-                  this.state.selected == index ? selectedColor : textColor
+                  this.state.selected === index ? selectedColor || fgColor
+                  : textColor || fgColorInverse
               },
               imageStyle
             ]}
@@ -169,12 +195,14 @@ export default class SwitchSelector extends Component {
           style={[
             {
               fontSize,
-              fontWeight: bold ? "bold" : "normal",
-              textAlign: "center",
-              color: this.state.selected == index ? selectedColor : textColor,
-              backgroundColor: "transparent"
+              lineHeight,
+              fontFamily: textFontFamily || fFamily,
+              textAlign: 'center',
+              color: this.state.selected === index ? selectedColor || fgColor
+              : textColor || fgColorInverse,
+              backgroundColor: 'transparent'
             },
-            this.state.selected == index ? selectedTextStyle : textStyle
+            this.state.selected === index ? selectedTextStyle : textStyle
           ]}
         >
           {element.label}
@@ -183,53 +211,47 @@ export default class SwitchSelector extends Component {
     ));
 
     return (
-      <View style={[{ flexDirection: "row" }, style]}>
+      <View style={[{ flexDirection: 'row' }, style]}>
         <View {...this._panResponder.panHandlers} style={{ flex: 1 }}>
           <View
             style={{
-              borderRadius: borderRadius,
-              backgroundColor: backgroundColor,
-              height
+              borderRadius: rounded ? height / 2 : borderRadius,
+              backgroundColor: backgroundColor || fgColor,
+              height: tight ? height * (2.2 / 3) : height
             }}
             onLayout={event => {
               const { width } = event.nativeEvent.layout;
               this.setState({
-                sliderWidth: width - (hasPadding ? 2 : 0)
+                sliderWidth: width
               });
             }}
           >
             <View
               style={{
                 flex: 1,
-                flexDirection: "row",
-                borderColor: borderColor || "#c9c9c9",
-                borderRadius: borderRadius,
-                borderWidth: hasPadding ? 1 : 0
+                flexDirection: 'row',
+                borderColor,
+                borderRadius: rounded ? height / 2 : borderRadius,
+                borderWidth
               }}
             >
               {!!this.state.sliderWidth && (
                 <Animated.View
                   style={[
                     {
-                      height: hasPadding ? height - 4 : height,
-                      backgroundColor: this.getBgColor(),
+                      height: tight ? height * (2.2 / 3) : height,
+                      backgroundColor: this.getBgColor(bgColor),
                       width:
-                        this.state.sliderWidth / this.props.options.length -
-                        (hasPadding ? valuePadding : 0),
+                        this.state.sliderWidth / this.props.options.length,
                       transform: [
                         {
                           translateX: this.animatedValue.interpolate({
                             inputRange: [0, 1],
-                            outputRange: [
-                              hasPadding ? valuePadding : 0,
-                              this.state.sliderWidth -
-                                (hasPadding ? valuePadding : 0)
-                            ]
+                            outputRange: [0, this.state.sliderWidth]
                           })
                         }
                       ],
-                      borderRadius: borderRadius,
-                      marginTop: hasPadding ? valuePadding : 0
+                      borderRadius: rounded ? height / 2 : borderRadius,
                     },
                     styles.animated
                   ]}
@@ -244,22 +266,12 @@ export default class SwitchSelector extends Component {
   }
 }
 
-SwitchSelector.defaultProps = {
+AwsmSelector.defaultProps = {
   style: {},
   textStyle: {},
   selectedTextStyle: {},
   imageStyle: {},
-  textColor: "#000000",
-  selectedColor: "#FFFFFF",
-  fontSize: 14,
-  backgroundColor: "#FFFFFF",
-  borderColor: "#C9C9C9",
-  borderRadius: 50,
-  hasPadding: false,
-  valuePadding: 1,
-  height: 40,
-  bold: false,
-  buttonColor: "#BCD635",
+  borderRadius: 13,
   returnObject: false,
   animationDuration: 100,
   disabled: false,
